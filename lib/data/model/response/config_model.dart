d8d0bcb8e3067af90bd594e7c71f1990944de2d3
techhnoob <techhnoob@gmail.com>
2023-09-28 08:03:17 +0530

    main branch hide

--- lib/data/model/response/config_model.dart
+++ lib/data/model/response/config_model.dart
@@ -37,7 +37,7 @@ class ConfigModel {
   List<String>? _activePaymentMethodList;
   double? _loyaltyPointItemPurchasePoint;
   bool? _loyaltyPointStatus;
-  double?  _loyaltyPointMinimumPoint;
+  double? _loyaltyPointMinimumPoint;
   double? _loyaltyPointExchangeRate;
   bool? _referEarningStatus;
   bool? _walletStatus;
@@ -46,60 +46,54 @@ class ConfigModel {
   int? _otpResendTime;
   bool? _isVegNonVegActive;
 
-
-
-
-
-
-  ConfigModel(
-      {String? restaurantName,
-        String? restaurantLogo,
-        String? restaurantAddress,
-        String? restaurantPhone,
-        String? restaurantEmail,
-        BaseUrls? baseUrls,
-        String? currencySymbol,
-        double? deliveryCharge,
-        String? cashOnDelivery,
-        String? digitalPayment,
-        String? termsAndConditions,
-        String? privacyPolicy,
-        String? aboutUs,
-        bool? emailVerification,
-        bool? phoneVerification,
-        String? currencySymbolPosition,
-        bool? maintenanceMode,
-        String? countryCode,
-        RestaurantLocationCoverage? restaurantLocationCoverage,
-        double? minimumOrderValue,
-        List<Branches?>? branches,
-        bool? selfPickup,
-        bool? homeDelivery,
-        DeliveryManagement? deliveryManagement,
-        PlayStoreConfig? playStoreConfig,
-        AppStoreConfig? appStoreConfig,
-        List<SocialMediaLink>? socialMediaLink,
-        String? softwareVersion,
-        String? footerCopyright,
-        String? timeZone,
-        int? decimalPointSettings,
-        List<RestaurantScheduleTime>? restaurantScheduleTime,
-        int? scheduleOrderSlotDuration,
-        String? timeFormat,
-        SocialStatus? socialLoginStatus,
-        List<String>? activePaymentMethodList,
-        double? loyaltyPointItemPurchasePoint,
-        bool? loyaltyPointStatus,
-        double? loyaltyPointMinimumPoint,
-        double? loyaltyPointExchangeRate,
-        bool? referEarningStatus,
-        bool? walletStatus,
-        Whatsapp? whatsapp,
-        CookiesManagement? cookiesManagement,
-        int? otpResendTime,
-        bool? isVegNonVegActive,
-
-      }) {
+  ConfigModel({
+    String? restaurantName,
+    String? restaurantLogo,
+    String? restaurantAddress,
+    String? restaurantPhone,
+    String? restaurantEmail,
+    BaseUrls? baseUrls,
+    String? currencySymbol,
+    double? deliveryCharge,
+    String? cashOnDelivery,
+    String? digitalPayment,
+    String? termsAndConditions,
+    String? privacyPolicy,
+    String? aboutUs,
+    bool? emailVerification,
+    bool? phoneVerification,
+    String? currencySymbolPosition,
+    bool? maintenanceMode,
+    String? countryCode,
+    RestaurantLocationCoverage? restaurantLocationCoverage,
+    double? minimumOrderValue,
+    List<Branches?>? branches,
+    bool? selfPickup,
+    bool? homeDelivery,
+    DeliveryManagement? deliveryManagement,
+    PlayStoreConfig? playStoreConfig,
+    AppStoreConfig? appStoreConfig,
+    List<SocialMediaLink>? socialMediaLink,
+    String? softwareVersion,
+    String? footerCopyright,
+    String? timeZone,
+    int? decimalPointSettings,
+    List<RestaurantScheduleTime>? restaurantScheduleTime,
+    int? scheduleOrderSlotDuration,
+    String? timeFormat,
+    SocialStatus? socialLoginStatus,
+    List<String>? activePaymentMethodList,
+    double? loyaltyPointItemPurchasePoint,
+    bool? loyaltyPointStatus,
+    double? loyaltyPointMinimumPoint,
+    double? loyaltyPointExchangeRate,
+    bool? referEarningStatus,
+    bool? walletStatus,
+    Whatsapp? whatsapp,
+    CookiesManagement? cookiesManagement,
+    int? otpResendTime,
+    bool? isVegNonVegActive,
+  }) {
     _restaurantName = restaurantName;
     _restaurantLogo = restaurantLogo;
     _restaurantAddress = restaurantAddress;
@@ -151,8 +145,6 @@ class ConfigModel {
     _cookiesManagement = cookiesManagement;
     _otpResendTime = otpResendTime;
     _isVegNonVegActive = isVegNonVegActive;
-
-
   }
 
   String? get restaurantName => _restaurantName;
@@ -166,9 +158,10 @@ class ConfigModel {
   String? get cashOnDelivery => _cashOnDelivery;
   String? get digitalPayment => _digitalPayment;
   String? get termsAndConditions => _termsAndConditions;
-  String? get aboutUs=> _aboutUs;
-  String? get privacyPolicy=> _privacyPolicy;
-  RestaurantLocationCoverage? get restaurantLocationCoverage => _restaurantLocationCoverage;
+  String? get aboutUs => _aboutUs;
+  String? get privacyPolicy => _privacyPolicy;
+  RestaurantLocationCoverage? get restaurantLocationCoverage =>
+      _restaurantLocationCoverage;
   double? get minimumOrderValue => _minimumOrderValue;
   List<Branches?>? get branches => _branches;
   bool? get emailVerification => _emailVerification;
@@ -184,9 +177,10 @@ class ConfigModel {
   List<SocialMediaLink>? get socialMediaLink => _socialMediaLink;
   String? get softwareVersion => _softwareVersion;
   String? get footerCopyright => _footerCopyright;
-  String? get timeZone  => _timeZone;
+  String? get timeZone => _timeZone;
   int? get decimalPointSettings => _decimalPointSettings;
-  List<RestaurantScheduleTime>? get restaurantScheduleTime => _restaurantScheduleTime;
+  List<RestaurantScheduleTime>? get restaurantScheduleTime =>
+      _restaurantScheduleTime;
   int? get scheduleOrderSlotDuration => _scheduleOrderSlotDuration;
   String? get timeFormat => _timeFormat;
   SocialStatus? get socialLoginStatus => _socialLoginStatus;
@@ -202,18 +196,14 @@ class ConfigModel {
   int? get otpResendTime => _otpResendTime;
   bool? get isVegNonVegActive => _isVegNonVegActive;
 
-
-
-
   ConfigModel.fromJson(Map<String, dynamic> json) {
     _restaurantName = json['restaurant_name'];
     _restaurantLogo = json['restaurant_logo'];
     _restaurantAddress = json['restaurant_address'];
     _restaurantPhone = json['restaurant_phone'];
     _restaurantEmail = json['restaurant_email'];
-    _baseUrls = json['base_urls'] != null
-        ? BaseUrls.fromJson(json['base_urls'])
-        : null;
+    _baseUrls =
+        json['base_urls'] != null ? BaseUrls.fromJson(json['base_urls']) : null;
     _currencySymbol = json['currency_symbol'];
     _deliveryCharge = json['delivery_charge'].toDouble();
     _cashOnDelivery = json['cash_on_delivery'];
@@ -229,12 +219,18 @@ class ConfigModel {
     _selfPickup = json['self_pickup'];
     _homeDelivery = json['delivery'];
     _restaurantLocationCoverage = json['restaurant_location_coverage'] != null
-        ? RestaurantLocationCoverage.fromJson(json['restaurant_location_coverage']) : null;
-    _minimumOrderValue = json['minimum_order_value'] != null ? json['minimum_order_value'].toDouble() : 0;
+        ? RestaurantLocationCoverage.fromJson(
+            json['restaurant_location_coverage'])
+        : null;
+    _minimumOrderValue = json['minimum_order_value'] != null
+        ? json['minimum_order_value'].toDouble()
+        : 0;
     if (json['branches'] != null) {
       _branches = [];
       json['branches'].forEach((v) {
-        _branches!.add(Branches.fromJson(v));
+        if (Branches.fromJson(v).id != 1) {
+          _branches!.add(Branches.fromJson(v));
+        }
       });
     }
     _deliveryManagement = json['delivery_management'] != null
@@ -253,53 +249,58 @@ class ConfigModel {
         _socialMediaLink!.add(SocialMediaLink.fromJson(v));
       });
     }
-    if(json['software_version'] !=null){
+    if (json['software_version'] != null) {
       _softwareVersion = json['software_version'];
     }
-    if(json['footer_text']!=null){
+    if (json['footer_text'] != null) {
       _footerCopyright = json['footer_text'];
     }
     _timeZone = json['time_zone'];
     _decimalPointSettings = json['decimal_point_settings'] ?? 1;
 
-    _restaurantScheduleTime = List<RestaurantScheduleTime>.from(json["restaurant_schedule_time"].map((x) => RestaurantScheduleTime.fromJson(x)));
+    _restaurantScheduleTime = List<RestaurantScheduleTime>.from(
+        json["restaurant_schedule_time"]
+            .map((x) => RestaurantScheduleTime.fromJson(x)));
 
     try {
       _scheduleOrderSlotDuration = json['schedule_order_slot_duration'] ?? 30;
-    }catch(_){
-      _scheduleOrderSlotDuration = int.tryParse(json['schedule_order_slot_duration'] ?? 30 as String);
+    } catch (_) {
+      _scheduleOrderSlotDuration =
+          int.tryParse(json['schedule_order_slot_duration'] ?? 30 as String);
     }
 
-    _timeFormat =  json['time_format'].toString();
+    _timeFormat = json['time_format'].toString();
 
-    if(json['social_login'] != null) {
-      _socialLoginStatus = SocialStatus.fromJson(json['social_login']) ;
+    if (json['social_login'] != null) {
+      _socialLoginStatus = SocialStatus.fromJson(json['social_login']);
     }
-    
-    if(json['active_payment_method_list'] != null) {
-      _activePaymentMethodList = json['active_payment_method_list'].cast<String>();
+
+    if (json['active_payment_method_list'] != null) {
+      _activePaymentMethodList =
+          json['active_payment_method_list'].cast<String>();
     }
 
     //json['active_payment_method_list'];
 
-   if(json['loyalty_point_item_purchase_point'] != null) {
-     _loyaltyPointItemPurchasePoint = double.parse('${json['loyalty_point_item_purchase_point']}');
-   }
-   _loyaltyPointStatus = '${json['loyalty_point_status']}' == '1';
-    _loyaltyPointMinimumPoint = double.tryParse('${json['loyalty_point_minimum_point']}');
-    _loyaltyPointExchangeRate = double.tryParse('${json['loyalty_point_exchange_rate']}');
+    if (json['loyalty_point_item_purchase_point'] != null) {
+      _loyaltyPointItemPurchasePoint =
+          double.parse('${json['loyalty_point_item_purchase_point']}');
+    }
+    _loyaltyPointStatus = '${json['loyalty_point_status']}' == '1';
+    _loyaltyPointMinimumPoint =
+        double.tryParse('${json['loyalty_point_minimum_point']}');
+    _loyaltyPointExchangeRate =
+        double.tryParse('${json['loyalty_point_exchange_rate']}');
     _referEarningStatus = '${json['ref_earning_status']}' == '1';
     _walletStatus = '${json['wallet_status']}' == '1';
-    _whatsapp = json['whatsapp'] != null
-        ? Whatsapp.fromJson(json['whatsapp'])
-        : null;
+    _whatsapp =
+        json['whatsapp'] != null ? Whatsapp.fromJson(json['whatsapp']) : null;
     _cookiesManagement = json['cookies_management'] != null
         ? CookiesManagement.fromJson(json['cookies_management'])
         : null;
 
-    _otpResendTime =  int.tryParse('${json['otp_resend_time']}');
+    _otpResendTime = int.tryParse('${json['otp_resend_time']}');
     _isVegNonVegActive = '${json['is_veg_non_veg_active']}'.contains('1');
-
   }
 
   Map<String, dynamic> toJson() {
@@ -327,7 +328,8 @@ class ConfigModel {
     data['self_pickup'] = selfPickup;
     data['delivery'] = homeDelivery;
     if (_restaurantLocationCoverage != null) {
-      data['restaurant_location_coverage'] = _restaurantLocationCoverage!.toJson();
+      data['restaurant_location_coverage'] =
+          _restaurantLocationCoverage!.toJson();
     }
     data['minimum_order_value'] = _minimumOrderValue;
     if (_branches != null) {
@@ -377,19 +379,19 @@ class BaseUrls {
   String? _chatImageUrl;
   String? _branchImageUrl;
 
-  BaseUrls(
-      {String? productImageUrl,
-        String? customerImageUrl,
-        String? bannerImageUrl,
-        String? categoryImageUrl,
-        String? categoryBannerImageUrl,
-        String? reviewImageUrl,
-        String? notificationImageUrl,
-        String? restaurantImageUrl,
-        String? deliveryManImageUrl,
-        String? chatImageUrl,
-        String? branchImageUrl,
-      }) {
+  BaseUrls({
+    String? productImageUrl,
+    String? customerImageUrl,
+    String? bannerImageUrl,
+    String? categoryImageUrl,
+    String? categoryBannerImageUrl,
+    String? reviewImageUrl,
+    String? notificationImageUrl,
+    String? restaurantImageUrl,
+    String? deliveryManImageUrl,
+    String? chatImageUrl,
+    String? branchImageUrl,
+  }) {
     _productImageUrl = productImageUrl;
     _customerImageUrl = customerImageUrl;
     _bannerImageUrl = bannerImageUrl;
@@ -488,18 +490,18 @@ class Branches {
   String? _image;
   bool? _status;
 
-  Branches(
-      {int? id,
-        String? name,
-        String? email,
-        String? longitude,
-        String? latitude,
-        String? address,
-        double? coverage,
-        String? coverImage,
-        String? image,
-        bool? status,
-      }) {
+  Branches({
+    int? id,
+    String? name,
+    String? email,
+    String? longitude,
+    String? latitude,
+    String? address,
+    double? coverage,
+    String? coverImage,
+    String? image,
+    bool? status,
+  }) {
     _id = id;
     _name = name;
     _email = email;
@@ -533,7 +535,6 @@ class Branches {
     _image = json['image'];
     _status = '${json['status']}'.contains('1');
     _coverImage = json['cover_image'];
-
   }
 
   Map<String, dynamic> toJson() {
@@ -550,6 +551,7 @@ class Branches {
     return data;
   }
 }
+
 class BranchValue {
   final Branches? branches;
   final double distance;
@@ -587,26 +589,27 @@ class DeliveryManagement {
     return data;
   }
 }
-class PlayStoreConfig{
+
+class PlayStoreConfig {
   bool? _status;
   String? _link;
   double? _minVersion;
 
-  PlayStoreConfig({bool? status, String? link, double? minVersion}){
+  PlayStoreConfig({bool? status, String? link, double? minVersion}) {
     _status = status;
     _link = link;
     _minVersion = minVersion;
   }
   bool? get status => _status;
   String? get link => _link;
-  double? get minVersion =>_minVersion;
+  double? get minVersion => _minVersion;
 
   PlayStoreConfig.fromJson(Map<String, dynamic> json) {
     _status = json['status'];
-    if(json['link'] != null){
+    if (json['link'] != null) {
       _link = json['link'];
     }
-    if(json['min_version'] != null && json['min_version'] != '' ){
+    if (json['min_version'] != null && json['min_version'] != '') {
       _minVersion = double.parse(json['min_version']);
     }
   }
@@ -620,12 +623,12 @@ class PlayStoreConfig{
   }
 }
 
-class AppStoreConfig{
+class AppStoreConfig {
   bool? _status;
   String? _link;
   double? _minVersion;
 
-  AppStoreConfig({bool? status, String? link, double? minVersion}){
+  AppStoreConfig({bool? status, String? link, double? minVersion}) {
     _status = status;
     _link = link;
     _minVersion = minVersion;
@@ -633,18 +636,16 @@ class AppStoreConfig{
 
   bool? get status => _status;
   String? get link => _link;
-  double? get minVersion =>_minVersion;
-
+  double? get minVersion => _minVersion;
 
   AppStoreConfig.fromJson(Map<String, dynamic> json) {
     _status = json['status'];
-    if(json['link'] != null){
+    if (json['link'] != null) {
       _link = json['link'];
     }
-    if(json['min_version'] !=null  && json['min_version'] != ''){
+    if (json['min_version'] != null && json['min_version'] != '') {
       _minVersion = double.parse(json['min_version']);
     }
-
   }
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
@@ -663,12 +664,7 @@ class SocialMediaLink {
   int? status;
   String? updatedAt;
 
-  SocialMediaLink(
-      {this.id,
-        this.name,
-        this.link,
-        this.status,
-        this.updatedAt});
+  SocialMediaLink({this.id, this.name, this.link, this.status, this.updatedAt});
 
   SocialMediaLink.fromJson(Map<String, dynamic> json) {
     id = json['id'];
@@ -700,26 +696,27 @@ class RestaurantScheduleTime {
   String? openingTime;
   String? closingTime;
 
-  factory RestaurantScheduleTime.fromJson(Map<String, dynamic> json) => RestaurantScheduleTime(
-    day: json["day"].toString(),
-    openingTime: json["opening_time"].toString(),
-    closingTime: json["closing_time"].toString(),
-  );
+  factory RestaurantScheduleTime.fromJson(Map<String, dynamic> json) =>
+      RestaurantScheduleTime(
+        day: json["day"].toString(),
+        openingTime: json["opening_time"].toString(),
+        closingTime: json["closing_time"].toString(),
+      );
 
   Map<String, dynamic> toJson() => {
-    "day": day,
-    "opening_time": openingTime,
-    "closing_time": closingTime,
-  };
+        "day": day,
+        "opening_time": openingTime,
+        "closing_time": closingTime,
+      };
 }
 
-class SocialStatus{
+class SocialStatus {
   bool? isGoogle;
   bool? isFacebook;
 
   SocialStatus(this.isGoogle, this.isFacebook);
 
-  SocialStatus.fromJson(Map<String, dynamic> json){
+  SocialStatus.fromJson(Map<String, dynamic> json) {
     isGoogle = '${json['google']}' == '1';
     isFacebook = '${json['facebook']}' == '1';
   }
@@ -750,7 +747,6 @@ class Whatsapp {
     return data;
   }
 }
-
 
 class CookiesManagement {
   bool? status;
