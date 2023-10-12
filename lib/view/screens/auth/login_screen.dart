20801c8b18de3d1fd2efdee3641c3e5769d66210
techhnoob <techhnoob@gmail.com>
2023-10-12 22:37:17 +0530

    login screen branch removed

--- lib/view/screens/auth/login_screen.dart
+++ lib/view/screens/auth/login_screen.dart
@@ -402,7 +402,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                             }
                                             Navigator.pushNamedAndRemoveUntil(
                                                 context,
-                                                Routes.getBranchListScreen(),
+                                                Routes.getMainRoute(),
                                                 (route) => false);
                                           }
                                         });
